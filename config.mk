0f1fd5aaa225b49f8b3b5f641c664f5b7065feb3
Francois Laupretre <francois.laupretre@externe.bnpparibas.com>
2014-06-10 17:45:03 +0200

    Modify build system - Remove autoconf stuff and use basic Makefiles.

---
+++ config.mk
@@ -0,0 +1,24 @@
+
+#----------------------------------------------------------------------------
+# Configurable parameters
+
+# The command to run the PHP CLI interpreter
+
+PHP = php
+
+# The command to run phpdoc
+
+PHPDOC = phpdoc
+
+# The command to run phpxref
+
+PHPXREF = /opt/phpxref/phpxref.pl
+
+#----------------------------------------------------------------------------
+# Non Configurable parameters
+
+SOFTWARE_VERSION = 1.3
+
+SOFTWARE_RELEASE = 1
+
+#----------------------------------------------------------------------------
