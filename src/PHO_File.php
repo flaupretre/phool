851de2922c2ba41e01c23c1b5c9d4d3f4ab18ba0
Francois Laupretre <francois@tekwire.net>
2014-12-04 18:41:29 +0100

    PHO_File: Incorporate changes from Automap
    Modified combine_path()
    New methods : trailing_separ, is_absolute_path, mk_absolute_path.

--- src/PHO_File.php
+++ src/PHO_File.php
@@ -21,27 +21,83 @@ if ($dotpos===false) return '';
 return strtolower(substr($filename,$dotpos+1));
 }
 
-//---------
-// Combines a base directory and a relative path. If the base directory is
-// '.', returns the relative part without modification
-// Use '/' separator on stream-wrapper URIs
+//----
+/**
+* Combines a base path with another path
+*
+* The base path can be relative or absolute.
+*
+* The 2nd path can also be relative or absolute. If absolute, it is returned
+* as-is. If it is a relative path, it is combined to the base path.
+*
+* Uses '/' as separator (to be compatible with stream-wrapper URIs).
+*
+* @param string $base The base path
+* @param string|null $path The path to combine
+* @param bool $separ true: add trailing sep, false: remove it
+* @return string The resulting path
+*/
 
-public static function combine_path($dir,$rpath)
+public static function combine_path($base,$path,$separ=false)
 {
-if ($dir=='.' || $dir=='') return $rpath;
-if ($rpath==='' || $rpath==='.') return $dir;
-$rpath=trim($rpath,'/');
-$rpath=trim($rpath,'\\');
+if (($base=='.') || ($base=='') || self::is_absolute_path($path))
+	$res=$path;
+elseif (($path=='.') || is_null($path))
+	$res=$base;
+else	//-- Relative path : combine it to base
+	$res=rtrim($base,'/\\').'/'.$path;
 
-$separ=(strpos($dir,':')!==false) ? '/' : DIRECTORY_SEPARATOR;
-if (($dir==='/') || ($dir==='\\')) $separ='';
-else
-	{
-	$c=substr($dir,-1,1);
-	if (($c==='/') || ($c=='\\')) $dir=rtrim($dir,$c);
-	}
+return self::trailing_separ($res,$separ);
+}
 
-return $dir.$separ.$rpath;
+//---------------------------------
+/**
+* Adds or removes a trailing separator in a path
+*
+* @param string $path Input
+* @param bool $flag true: add trailing sep, false: remove it
+* @return bool The result path
+*/
+
+public static function trailing_separ($path, $separ)
+{
+$path=rtrim($path,'/\\');
+if ($path=='') return '/';
+if ($separ) $path=$path.'/';
+return $path;
+}
+
+//---------------------------------
+/**
+* Determines if a given path is absolute or relative
+*
+* @param string $path The path to check
+* @return bool True if the path is absolute, false if relative
+*/
+
+public static function is_absolute_path($path)
+{
+return ((strpos($path,':')!==false)
+	||(strpos($path,'/')===0)
+	||(strpos($path,'\\')===0));
+}
+
+//---------------------------------
+/**
+* Build an absolute path from a given (absolute or relative) path
+*
+* If the input path is relative, it is combined with the current working
+* directory.
+*
+* @param string $path The path to make absolute
+* @param bool $separ True if the resulting path must contain a trailing separator
+* @return string The resulting absolute path
+*/
+
+public static function mk_absolute_path($path,$separ=false)
+{
+if (!self::is_absolute_path($path)) $path=self::combine_path(getcwd(),$path);
+return self::trailing_separ($path,$separ);
 }
 
 //---------
@@ -79,7 +135,7 @@ if (file_put_contents($tmpf,$data)!=strlen($data))
 
 // Windows does not support renaming to an existing file (looses atomicity)
 
-if (PHK_Util::is_windows()) @unlink($path);
+if (PHO_Util::env_is_windows()) @unlink($path);
 
 if (!rename($tmpf,$path))
 	{
